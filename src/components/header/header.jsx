0fc3aa2db70ec6afc7fcd318df486a216d6844c7
vishal-jpeg <65063439+vishal-jpeg@users.noreply.github.com>
2024-02-15 16:58:37 +0530

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -13,7 +13,7 @@ const header = () => {
       <div className="container header__container">
         <h5>Hello I`m</h5>
         <h1>Vishal</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className='text-light'>Developer</h5>
         <CTA />
         <HeaderSocial/>
 
